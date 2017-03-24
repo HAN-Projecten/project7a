be205aa5ec32553ef2c34234a8b659bea27342ce
Martijn Landman <martijn.landman@hotmail.nl>
2017-03-24 13:42:25 +0100

    voorspellenORF zelfstandig werkzaam gemaakt

--- project_7a/src/project_7a/VoorspellenORF.java
+++ project_7a/src/project_7a/VoorspellenORF.java
@@ -6,7 +6,10 @@
 package project_7a;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -14,12 +17,46 @@ import java.util.HashMap;
  */
 public class VoorspellenORF {
     
-    private ArrayList<String> startCodons;
-    private ArrayList<String> stopCodons;
-    private ArrayList<ORF> ORFs;
+    private static final List<String> startCodons = Arrays.asList("AUG","UUG","GUG");
+    private static final List<String> stopCodons = Arrays.asList("UAA","UGA","UAG");
+    private List<ORF> ORFs = new ArrayList<ORF>();
     
-    public void voorspelORF(HashMap nucleotideSequentie) {
-        
+    public static void main(String[] args) {
+        HashMap stuff = new HashMap();
+        stuff.put("1+", "atcAUGuuuaaauuuUGAtttUUGcccaaacccUAG");
+        new VoorspellenORF().voorspelORF(stuff);
     }
     
+    public void voorspelORF(HashMap<String,String> nucleotideSequenties) {
+        String readingFrame;
+        String sequentie;
+        int begin = 0;
+        int eind;
+        boolean start = false;
+        String codon = null;
+        for (Map.Entry<String,String> entry : nucleotideSequenties.entrySet()) {
+            readingFrame = entry.getKey();
+            sequentie = entry.getValue();
+            for (int i = 0; i < sequentie.length(); i+=3) {
+                try {
+                    codon = sequentie.substring(i, i+3).toUpperCase();
+                    System.out.println(codon);
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("OUT OF BOUNDS");
+                }
+                if (!start && startCodons.contains(codon)) {
+                    start = true;
+                    begin = i+1;
+                }
+                else if (start && stopCodons.contains(codon)) {
+                    start = false;
+                    eind = i+3;
+                    ORF temp = new ORF(begin, eind, readingFrame, sequentie);
+                    ORFs.add(temp);
+                }
+            }
+        }
+        System.out.println(ORFs);
+    }
+
 }
