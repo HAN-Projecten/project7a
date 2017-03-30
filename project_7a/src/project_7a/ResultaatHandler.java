08dfb4f39899f4d0d32bff49e5d306ce39d0c44e
Marliek <M.Bonnecroij@student.han.nl>
2017-03-30 15:31:01 +0200

    opslaan logica uitgebreidt

--- project_7a/src/project_7a/ResultaatHandler.java
+++ project_7a/src/project_7a/ResultaatHandler.java
@@ -8,15 +8,19 @@ package project_7a;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
 /**
  *
  * @author Marliek
  */
 public class ResultaatHandler {
-    static String nucleotideSequentie = "atgttaaaagctataagtagatcaaagatagcaatcaatctgattcgttattcatcaacagctagcaatagtccaaagt";
+    static int sequentie_id;
+    static int ORF_id;
+   
+    
     public static void main(String[] args) throws Exception{
-        opslaanResultaten();
+        opslaanSeq(nucleotideSequentie);
     }
     public static Connection getConnectie() throws Exception{
         try{
@@ -32,17 +36,34 @@ public class ResultaatHandler {
         }catch(Exception e){System.out.println(e);}
         return null;
     }
-    public static void opslaanResultaten() throws Exception{
+    public static void opslaanSeq(String nucleotideSequentie) throws Exception{
         try{
             Connection conn = getConnectie();
             String querySeq ="insert into sequentie(sequentie_id , invoer_sequentie)" + "values (?,?)";
-            PreparedStatement preparedStmt = conn.prepareStatement(querySeq);
-            preparedStmt.setInt(1, 1);
-            preparedStmt.setString(2, nucleotideSequentie);
-            preparedStmt.execute();
-      
+            PreparedStatement preparedQuery = conn.prepareStatement(querySeq);
+            preparedQuery.setInt(0,sequentie_id );
+            preparedQuery.setString(1, nucleotideSequentie);
+            preparedQuery.execute();
+            
             conn.close();
         }catch(Exception e){System.out.println(e);}
-        
+    }
+    public static void opslaanORF(ArrayList<ORF> ORFs) throws Exception{
+        try{
+            Connection conn = getConnectie();
+            for (ORF ORF : ORFs) {
+                String queryORF = "insert into orf (ORF_id, ORF_sequentie, ORF_lengte, reading_frame, begin, eind, Sequentie_sequentie_id)" + "values (?,?,?,?,?,?,?)";
+                PreparedStatement preparedORF = conn.prepareStatement(queryORF);
+                preparedORF.setInt(0, ORF_id);
+                preparedORF.setString(1, ORF.getSequentie()); 
+                preparedORF.setInt(2, ORF.getLengte());
+                preparedORF.setString(3, ORF.getReadingFrame());
+                preparedORF.setInt(4, ORF.getBegin());
+                preparedORF.setInt(5, ORF.getEind());
+                preparedORF.setInt(6, sequentie_id);
+                preparedORF.execute();
+            }
+            conn.close();
+        }catch(Exception e){System.out.println(e);}      
     }
 }
